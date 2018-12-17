05012d15edc7440f023e6393b38e536e0777b408
alvin farkhan <alvin.farkhan@gmail.com>
2018-12-18 00:39:12 +0700

    add paths

--- bliblinventory/src/main/java/com/sal/bliblinventory/config/WebConfig.java
+++ bliblinventory/src/main/java/com/sal/bliblinventory/config/WebConfig.java
@@ -15,6 +15,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/homeEmployee").setViewName("homeEmployee.html");
         registry.addViewController("/homeSuperior").setViewName("homeSuperior.html");
         registry.addViewController("/homeAdmin").setViewName("homeAdmin.html");
+        registry.addViewController("/daftarBarangCard.html").setViewName("daftarBarangCard.html");
+        registry.addViewController("/orderList.html").setViewName("orderList.html");
+        registry.addViewController("/daftarBarangCard.html").setViewName("daftarBarangCard.html");
+        registry.addViewController("/daftarKaryawan.html").setViewName("daftarKaryawan.html");
+        registry.addViewController("/permintaanKaryawan.html").setViewName("permintaanKaryawan.html");
+
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 }

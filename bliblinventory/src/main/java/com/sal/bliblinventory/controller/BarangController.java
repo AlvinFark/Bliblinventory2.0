9f7d8f9561125089b3f12ecd0d522bcd356aa4d9
sylviaputri <sylvi.bluemochi@gmail.com>
2018-12-20 22:30:01 +0700

    fixed sorting and searching product in emplyee page

--- bliblinventory/src/main/java/com/sal/bliblinventory/controller/BarangController.java
+++ bliblinventory/src/main/java/com/sal/bliblinventory/controller/BarangController.java
@@ -4,10 +4,7 @@ import com.sal.bliblinventory.model.Barang;
 import com.sal.bliblinventory.service.BarangService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,26 +18,38 @@ public class BarangController {
     }
 
     @GetMapping(value = "employee/sortByName")
-    public List<Barang> listBarang(Model md){
+    public List<Barang> listBarangSortByName(Model md){
         List<Barang> barang = barangService.sortByName();
         return barang;
     }
 
+    @RequestMapping(value = "employee/sortByName/{param1}", method = RequestMethod.GET)
+    public List<Barang> listBarangSortByNameAndSeachKeyword(Model md, @PathVariable(value = "param1") String param1){
+        List<Barang> barang = barangService.sortByNameAndSeachKeyword(param1);
+        return barang;
+    }
+
     @GetMapping(value = "employee/sortByCode")
-    public List<Barang> listBarangByCode(Model md){
+    public List<Barang> listBarangSortByCode(Model md){
         List<Barang> barang = barangService.sortByCode();
+        return barang;
+    }
+
+    @RequestMapping(value = "employee/sortByCode/{param1}", method = RequestMethod.GET)
+    public List<Barang> listBarangSortByCodeAndSeachKeyword(Model md, @PathVariable(value = "param1") String param1){
+        List<Barang> barang = barangService.sortByCodeAndSeachKeyword(param1);
         return barang;
     }
 
     @RequestMapping(value = "employee/getAllProduct", method = RequestMethod.GET)
     public List<Barang> listBarangAll(Model md){
-        List<Barang> barang = barangService.sortByCode();
+        List<Barang> barang = barangService.sortByName();
         return barang;
     }
-    /*
-    public String listBarangEmployee(Model md){
-        md.addAttribute("barang", barangService.findAll());
 
-        return "daftarBarangCard";
-    }*/
+    @RequestMapping(value = "employee/getDetailProduct/{param1}", method = RequestMethod.GET)
+    public Barang detailBarang(Model md, @PathVariable(value = "param1") String param1){
+        List<Barang> barang = barangService.getDetailBarang(param1);
+        return barang.get(0);
+    }
 }

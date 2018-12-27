bfcd539dce0e63b1b55e4598bab13eb8fd77ae41
alvin farkhan <alvin.farkhan@gmail.com>
2018-12-27 17:04:56 +0700

    list barang admin (bug: cant make custom id)

--- bliblinventory/src/main/resources/static/JS/daftarBarangTable.js
+++ bliblinventory/src/main/resources/static/JS/daftarBarangTable.js
@@ -3,6 +3,8 @@ $( document ).ready(function() {
     $('.modal').modal();
     $('.datepicker').datepicker();
 
+  ajaxGetBarangTable('',0);
+
     $(".clickDetailTable").click(function(){
         $("#editBarangTable").hide();
         $("#tambahSatuanTable").hide();
@@ -35,6 +37,64 @@ $( document ).ready(function() {
         $("#tombolEditBarang").hide();
         $("#clickTambahSatuan").hide();
     })
-    
+
+    function ajaxGetBarangTable(keyword, filter) {
+      $.ajax({
+        type : "GET",
+        url : "employee/sortByName/"+keyword,
+        success: function(result){
+          $("#tabelDaftarBarang").html('');
+          for(var i = 0; i < result.length; i++){
+            var kategoriBarang=((result[i] || {}).category || {}).name;
+            var totalSubBarang;
+            var subBarangTersedia;
+            $("#tabelDaftarBarang").append('' +
+              '<tr>\n' +
+              '  <td><p><label><input id="checkBoxBarang'+ i +'" type="checkbox"/><span></span></label></p></td>\n' +
+              '  <td>'+ result[i].kode +'</td>\n' +
+              '  <td>'+ result[i].nama +'</td>\n' +
+              '  <td>'+ result[i].kategoriBarang +'</td>\n' +
+              '  <td>'+ result[i].hargaBeli +'</td>\n' +
+              '  <td id="totalSubBarangTable">'+ totalSubBarang +'</td>\n' +
+              '  <td id="subBarangTersediaTable">'+ subBarangTersedia +'</td>\n' +
+              '  <td width=100px>\n' +
+              '    <a class="waves-effect waves-light btn right modal-trigger kotak-small clickEditTable" href="#modalDetailBarangTable"><i class="material-icons">edit</i></a>\n' +
+              '    <a class="waves-effect waves-light btn right modal-trigger kotak-small clickDetailTable" href="#modalDetailBarangTable"><i class="material-icons">search</i></a>\n' +
+              '  </td>\n' +
+              '</tr>\n')
+            $.ajax({
+              type : "GET",
+              url : "/employee/countAllSubBarang/" +result[i].kode,
+              success: function(result1){
+                totalSubBarang = result1;
+                $("#totalSubBarangTable").html(result1);
+              }
+            });
+            $.ajax({
+              type : "GET",
+              url : "/employee/countReadySubBarang/" +result[i].kode,
+              success: function(result2){
+                subBarangTersedia = result2;
+                $("#subBarangTersediaTable").html(result2);
+              }
+            });
+          }
+        }
+      })
+    }
+
+    $("#searchBarang").keypress(function(e) {
+      if(e.which == 13) {
+        ajaxGetBarangTable($("#searchBarang").val(),$("#filterBarang").prop('selectedIndex'));
+      }
+    });
+  
+    $( document ).on("change","#filterBarang",function (){
+      ajaxGetBarangTable($("#searchBarang").val(),$("#filterBarang").prop('selectedIndex'));
+    });
+  
+    $("#btnRefreshListList").click(function() {
+      ajaxGetBarangTable($("#searchBarang").val(),$("#filterBarang").prop('selectedIndex'));
+    });
 
 });

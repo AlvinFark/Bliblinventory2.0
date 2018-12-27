93efa6872fde185ba84672341aecd5905790b0f5
alvin farkhan <alvin.farkhan@gmail.com>
2018-12-27 23:32:53 +0700

    modals

--- bliblinventory/src/main/resources/static/JS/daftarBarangTable.js
+++ bliblinventory/src/main/resources/static/JS/daftarBarangTable.js
@@ -5,6 +5,15 @@ $( document ).ready(function() {
 
   ajaxGetBarangTable('',0);
 
+  $("#editBarangTable").hide();
+  $("#tambahSatuanTable").hide();
+  $("#detailBarangTable").show();
+  $("#backToDetailTable").hide();
+  $("#tombolSimpanEditan").hide();
+  $("#tombolSimpanSatuan").hide();
+  $("#tombolEditBarang").show();
+  $("#clickTambahSatuan").show();
+
     $(".clickDetailTable").click(function(){
         $("#editBarangTable").hide();
         $("#tambahSatuanTable").hide();
@@ -25,7 +34,9 @@ $( document ).ready(function() {
         $("#tombolSimpanSatuan").hide();
         $("#tombolEditBarang").hide();
         $("#clickTambahSatuan").hide();
-    })
+    });
+
+    var indexSatuan = 0;
 
     $("#clickTambahSatuan").click(function(){
         $("#editBarangTable").hide();
@@ -36,52 +47,70 @@ $( document ).ready(function() {
         $("#tombolSimpanSatuan").show();
         $("#tombolEditBarang").hide();
         $("#clickTambahSatuan").hide();
-    })
+    });
 
     function ajaxGetBarangTable(keyword, filter) {
       $.ajax({
         type : "GET",
         url : "employee/sortByName/"+keyword,
-        success: function(result){
+        success: function(result) {
           $("#tabelDaftarBarang").html('');
-          for(var i = 0; i < result.length; i++){
-            var kategoriBarang=((result[i] || {}).category || {}).name;
+          for (var i = 0; i < result.length; i++) {
+            var kategoriBarang;
+            var idKategoriBarang = ((result[i] || {}).category || {}).id;
+            switch (idKategoriBarang) {
+              case 1 : {kategoriBarang="Elektronik"; break;}
+              case 2 : {kategoriBarang="Perkakas Kantor"; break;}
+            }
             var totalSubBarang;
             var subBarangTersedia;
-            $("#tabelDaftarBarang").append('' +
+            document.getElementById("tabelDaftarBarang").innerHTML += '' +
               '<tr>\n' +
-              '  <td><p><label><input id="checkBoxBarang'+ i +'" type="checkbox"/><span></span></label></p></td>\n' +
-              '  <td>'+ result[i].kode +'</td>\n' +
-              '  <td>'+ result[i].nama +'</td>\n' +
-              '  <td>'+ result[i].kategoriBarang +'</td>\n' +
-              '  <td>'+ result[i].hargaBeli +'</td>\n' +
-              '  <td id="totalSubBarangTable">'+ totalSubBarang +'</td>\n' +
-              '  <td id="subBarangTersediaTable">'+ subBarangTersedia +'</td>\n' +
+              '  <td><p><label><input id="checkBoxBarang' + i + '" type="checkbox"/><span></span></label></p></td>\n' +
+              '  <td>' + result[i].kode + '</td>\n' +
+              '  <td>' + result[i].nama + '</td>\n' +
+              '  <td>' + kategoriBarang + '</td>\n' +
+              '  <td>' + result[i].hargaBeli + '</td>\n' +
+              '  <td id="totalSubBarang' + i + '">a</td>\n' +
+              '  <td id="subBarangTersedia' + i + '">a</td>\n' +
               '  <td width=100px>\n' +
               '    <a class="waves-effect waves-light btn right modal-trigger kotak-small clickEditTable" href="#modalDetailBarangTable"><i class="material-icons">edit</i></a>\n' +
               '    <a class="waves-effect waves-light btn right modal-trigger kotak-small clickDetailTable" href="#modalDetailBarangTable"><i class="material-icons">search</i></a>\n' +
               '  </td>\n' +
-              '</tr>\n')
-            $.ajax({
-              type : "GET",
-              url : "/employee/countAllSubBarang/" +result[i].kode,
-              success: function(result1){
-                totalSubBarang = result1;
-                $("#totalSubBarangTable").html(result1);
-              }
-            });
-            $.ajax({
-              type : "GET",
-              url : "/employee/countReadySubBarang/" +result[i].kode,
-              success: function(result2){
-                subBarangTersedia = result2;
-                $("#subBarangTersediaTable").html(result2);
-              }
-            });
+              '</tr>\n';
           }
+          setTimeout(function () {
+            for (var i = 0; i < result.length; i++) {
+              $.ajax({
+                type: "GET",
+                url: "/employee/countAllSubBarang/" + result[i].kode,
+                success: function (result1) {
+                  $("td[id=" + "totalSubBarang" + i + "]").append(result1);
+                }
+              });
+              $.ajax({
+                type: "GET",
+                url: "/employee/countReadySubBarang/" + result[i].kode,
+                success: function (result2) {
+                  $("td[id=" + "subBarangTersedia" + i + "]").append(result2);
+                }
+              });
+            }
+          }, 5000);
         }
-      })
+      });
+
     }
+
+    $("#triggerTambahBarangSatuan").click(function () {
+      indexSatuan++;
+      $("#tambahBarangSatuan").append('' +
+        '  <div class="valign-wrapper" style="margin-top: 5px">\n' +
+        '    <div class="tdAtrib" style="width:100px;">ID Satuan</div>\n' +
+        '    <textarea id="barangSatuan'+ indexSatuan +'" name="satuan" cols="30" rows="1" style="height:30px; padding: 5px 0 0 5px;"></textarea>\n' +
+        '  </div>\n');
+    });
+
 
     $("#searchBarang").keypress(function(e) {
       if(e.which == 13) {
@@ -96,5 +125,7 @@ $( document ).ready(function() {
     $("#btnRefreshListList").click(function() {
       ajaxGetBarangTable($("#searchBarang").val(),$("#filterBarang").prop('selectedIndex'));
     });
+
+
 
 });

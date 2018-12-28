dfbed0a73efdc79b25da53b05fd2c67bc6703c0a
alvin farkhan <alvin.farkhan@gmail.com>
2018-12-28 16:25:36 +0700

    add subbarang ready

--- bliblinventory/src/main/java/com/sal/bliblinventory/controller/SubBarangController.java
+++ bliblinventory/src/main/java/com/sal/bliblinventory/controller/SubBarangController.java
@@ -1,7 +1,9 @@
 package com.sal.bliblinventory.controller;
 
 import com.sal.bliblinventory.exception.ResourceNotFoundException;
+import com.sal.bliblinventory.model.Barang;
 import com.sal.bliblinventory.model.SubBarang;
+import com.sal.bliblinventory.repository.BarangRepository;
 import com.sal.bliblinventory.repository.SubBarangRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -14,6 +16,9 @@ import java.util.List;
 public class SubBarangController {
     @Autowired
     SubBarangRepository subBarangRepository;
+
+    @Autowired
+    BarangRepository barangRepository;
 
     @RequestMapping(value = {"employee/countAllSubBarang/{param1}", "superior/countAllSubBarang/{param1}"}, method = RequestMethod.GET)
     public int countSubBarang(@PathVariable(value = "param1") String param1){
@@ -31,5 +36,14 @@ public class SubBarangController {
         subBarangToUpdate.setStatusSubBarang(false);
         subBarangRepository.save(subBarangToUpdate);
         return 1;
+    }
+
+    @RequestMapping(value = {"/api/barang/{kodebarang}/{kodesubbarang}"}, method = RequestMethod.POST)
+    public SubBarang addSubBarang(@PathVariable("kodebarang") String kodebarang, @PathVariable("kodesubbarang") String kodesubbarang) {
+
+      Barang barang = barangRepository.findBarangByKode(kodebarang);
+      SubBarang subBarangRequest = new SubBarang(kodesubbarang, barang);
+      subBarangRequest.setStatusSubBarang(true);
+      return subBarangRepository.save(subBarangRequest);
     }
 }
